8ef92325694c7c905845ff35f7ae7a2503ead825
Chisquare7 <chijiokechibuike.f@gmail.com>
2025-07-23 12:13:06 +0100

    feat: implement and verify Storage contract on Celo Alfajores - Added Storage.sol smart contract - Configured Hardhat for Celo Alfajores network - Deployed contract to Celo testnet - Verified contract on CeloScan using Hardhat Etherscan plugin

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    celoAlfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 44787,
+    },
+  },
+
+  etherscan: {
+    apiKey: "RBQ3VTM531MZGEDA9EHFFHTJKEXE59NA8A",
+    customChains: [
+      {
+        network: "celoAlfajores",
+        chainId: 44787,
+        urls: {
+          apiURL: "https://api-alfajores.celoscan.io/api",
+          browserURL: "https://alfajores.celoscan.io",
+        },
+      },
+    ],
+  },
+
+  sourcify: {
+    enabled: true,
+  },
   solidity: "0.8.28",
 };
--- README.md
+++ README.md
@@ -4,8 +4,12 @@
 
 ### Hardhat
 
-- Celo Alfajores: `0xYourDeployedAddressHere`
+- Celo Alfajores: `0x70520be67a91f116B6F1060dA05453a22c3c8E82`
 
 ### Foundry
 
 - Celo Alfajores: `0x07f61f5A1795A6349EA64E792E51FE503175e0Ce`
+
+### Hardhat Verified Contract:
+
+https://alfajores.celoscan.io/address/0x70520be67a91f116B6F1060dA05453a22c3c8E82#code
